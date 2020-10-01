31e6418debd5f187533f0dd12aff9a3d482d2cf2
Barry de Kleijn <kleijn.barry@gmail.com>
2020-10-01 22:58:36 +0200

    build #2

---
+++ docs/precache-manifest.e1c67e98a16089b8885f99bd61e22271.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36029d2f9a3368f6f7977fb1bf1772bd",
+    "url": "/milow6/index.html"
+  },
+  {
+    "revision": "6db38ccd022b53ad51fa",
+    "url": "/milow6/static/css/2.2a682f77.chunk.css"
+  },
+  {
+    "revision": "26d11971ab9df318a363",
+    "url": "/milow6/static/css/main.0ca4d17e.chunk.css"
+  },
+  {
+    "revision": "6db38ccd022b53ad51fa",
+    "url": "/milow6/static/js/2.b2cb75cd.chunk.js"
+  },
+  {
+    "revision": "0902ac8aef07907d405a054f5d3c6c13",
+    "url": "/milow6/static/js/2.b2cb75cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d11971ab9df318a363",
+    "url": "/milow6/static/js/main.05908725.chunk.js"
+  },
+  {
+    "revision": "843252e1f834f48cee9b",
+    "url": "/milow6/static/js/runtime-main.c2bbe3ac.js"
+  },
+  {
+    "revision": "53f4d4a2aeda0b0d2f3f414f6c007f73",
+    "url": "/milow6/static/media/dance.53f4d4a2.gif"
+  },
+  {
+    "revision": "2e43995626f798f402ad616b2828d71a",
+    "url": "/milow6/static/media/gotit.2e439956.gif"
+  },
+  {
+    "revision": "3d2f8f8147fa41438cf5bf396259a9e2",
+    "url": "/milow6/static/media/milow.3d2f8f81.jpg"
+  },
+  {
+    "revision": "5adecef31b2d35c191b424923a2ff8b8",
+    "url": "/milow6/static/media/sad.5adecef3.gif"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.61a1330df42f8bd6f8812cca35a29e5e.js"
+  "/milow6/precache-manifest.e1c67e98a16089b8885f99bd61e22271.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/milow6/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
